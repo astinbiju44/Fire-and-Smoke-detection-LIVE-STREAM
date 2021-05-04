9f8af43fbc6c5c6aeafd5eed981e44e2d3eecd4b
Astin Biju <astinbiju44@gmail.com>
2021-05-04 12:04:56 +0530

    feed from mobile added

--- FromCamera.py
+++ FromCamera.py
@@ -2,7 +2,5 @@ import cv2
 cam=cv2.VideoCapture(0)
 
 def camera():
-    while True:
         _,img=cam.read()
         return img
-    
--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 import cv2
 import numpy as np
+from FromCamera import camera
+from FromMobile import MobileCamera
+
+while True:
+    img=MobileCamera()
+    camimg=camera()
+    cv2.imshow("Camera Feed",camimg)
+    cv2.imshow("Mobile Feed", img)
+    cv2.waitKey(1)
 
