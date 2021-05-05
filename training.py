9c8326ae971635b416ec6ab146dc615a0fad4d0c
Astin Biju <astinbiju44@gmail.com>
2021-05-05 14:19:42 +0530

    defining model

--- training.py
+++ training.py
@@ -30,7 +30,7 @@ print("Total number image readed : "+str(len(imagefiles)))
 
 
 
-height,width,channels=96,96,3
+(height,width,channels)=(96,96,3)
 
 
 
@@ -52,7 +52,6 @@ for img in imagefiles:
     #print(label)
     labels.append([label]) #[[0],[2],[1],......]
 
-
 #data preprocessing
 data=np.array(data,dtype="float")/255.0  #it will convert into array of values between 0 & 1
 labels=np.array(labels)
@@ -62,6 +61,62 @@ labels=np.array(labels)
 (trainx,testx,trainy,testy)=train_test_split(data,labels,test_size=0.2,random_state=42)
 trainy=to_categorical(trainy,num_classes=3)
 testy=to_categorical(testy,num_classes=3)
+
+
+#augmenting dataset
+aug=ImageDataGenerator(rotation_range=25,width_shift_range=0.1,
+                       height_shift_range=0.1,shear_range=0.2,zoom_range=0.2,
+                       horizontal_flip=True,fill_mode="nearest")
+
+
+#defining model
+def buildmodel(width,height,depth,classes):
+    model=Sequential()
+    inputshape=(height,width,depth)
+    changeDim=-1
+
+    if k.image_data_format()=="channels_first": #return a string either 'Channels first' or 'chancels last'
+        inputshape=(depth,height,width)
+        changeDim = 1
+
+    model.add(Conv2D(filters=32, kernel_size=(3, 3),padding="same", activation='relu', input_shape=inputshape))
+    model.add(BatchNormalization(axis=changeDim))
+    model.add((MaxPooling2D(pool_size=(3, 3))))
+    model.add(Dropout(rate=0.25))
+
+    model.add(Conv2D(filters=64, kernel_size=(3, 3), padding="same",activation='relu'))
+    model.add(BatchNormalization(axis=changeDim))
+
+    model.add(Conv2D(filters=64, kernel_size=(3, 3), padding="same", activation='relu'))
+    model.add(BatchNormalization(axis=changeDim))
+    model.add((MaxPooling2D(pool_size=(2, 2))))
+    model.add(Dropout(rate=0.25))
+
+    model.add(Conv2D(filters=128, kernel_size=(3, 3), padding="same", activation='relu'))
+    model.add(BatchNormalization(axis=changeDim))
+
+    model.add(Conv2D(filters=128, kernel_size=(3, 3), padding="same", activation='relu'))
+    model.add(BatchNormalization(axis=changeDim))
+    model.add((MaxPooling2D(pool_size=(2, 2))))
+    model.add(Dropout(rate=0.25))
+
+    model.add(Flatten())
+    model.add(Dense(1024, activation='relu'))
+    model.add(BatchNormalization())
+    model.add(Dropout(rate=0.5))
+
+    model.add(Dense((classes), activation='softmax'))
+
+    return model
+
+model=buildmodel(width=width,height=height,depth=channels,classes=3)
+
+
+
+
+
+
+
 
 
 
