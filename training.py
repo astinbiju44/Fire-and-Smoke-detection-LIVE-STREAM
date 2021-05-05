ee8032de7116f70962ac5a70e32c0855e4388042
Astin Biju <astinbiju44@gmail.com>
2021-05-05 13:32:59 +0530

    converting images to array and labeling into categories

--- training.py
+++ training.py
@@ -15,6 +15,7 @@ import glob
 
 
 
+
 #To append images and its labels
 data=[]
 labels=[]
@@ -22,11 +23,37 @@ labels=[]
 
 
 
-
 #To load image files from the dataset
 imagefiles=[f for f in glob.glob(r'FIRE-SMOKE-DATASET'+"/**/*",recursive=True) if not os.path.isdir(f)]
 random.shuffle(imagefiles) #to balance the weight
-print(len(imagefiles))
+print("Total number image readed : "+str(len(imagefiles)))
+
+
+
+height,width,channels=96,96,3
+
+
+
+#converting images to array and labeling into categories
+for img in imagefiles:
+    #print(img)
+    image=cv2.imread(img)
+    image=cv2.resize(image,(96,96))
+    image=img_to_array(image)
+    data.append(image)
+
+    label=img.split(os.path.sep)[-2]  #every seperation is made whereever there is backslash and second last is the class name ie -2
+    if label=="Neutral":
+        label=0
+    elif label=="Fire":
+        label=1
+    else:
+        label=2
+    #print(label)
+    labels.append([label]) #[[0],[2],[1],......]
+
+
+
 
 
 
