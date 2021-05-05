cc142a62984d937b3d70dbb777d991900f340c73
Astin Biju <astinbiju44@gmail.com>
2021-05-05 12:47:54 +0530

    images loaded

--- training.py
+++ training.py
@@ -4,12 +4,32 @@ from tensorflow.keras.utils import to_categorical,plot_model
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import BatchNormalization,Conv2D,MaxPooling2D,Activation,Flatten,Dropout,Dense
 from tensorflow.keras import backend as k
-from  sklearn.model_selection import train_test_split
-import  matplotlib.pyplot as plt
-import  numpy as np
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import numpy as np
 import cv2
+import random
 import os
 import glob
+
+
+
+
+#To append images and its labels
+data=[]
+labels=[]
+
+
+
+
+
+#To load image files from the dataset
+imagefiles=[f for f in glob.glob(r'FIRE-SMOKE-DATASET'+"/**/*",recursive=True) if not os.path.isdir(f)]
+random.shuffle(imagefiles) #to balance the weight
+print(len(imagefiles))
+
+
+
 
 
 
